3087b760fd2007b7a0d0f4531b50c40b4b706105
sujonifty <sujonifty@gmail.com>
2024-04-02 19:24:39 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,29 @@
+import React, { Children } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root.jsx';
+import Home from './components/Home/Home.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    Children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ]
+  },
+
+]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
